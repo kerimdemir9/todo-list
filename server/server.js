9af96587d88d813f5d729f8289288239d8009fce
Kerim Demir <kerim.demir2002@hotmail.com>
2023-03-06 13:24:59 +0300

    server side implemented.

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const PORT = 5000;
+
+const app = express();
+
+app.get("/test", (req,res) => {
+    res.json({ok:"true"});
+})
+
+app.listen(PORT, () => console.log('Server is now listening on port ' + PORT))
